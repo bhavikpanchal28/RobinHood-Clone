92be7201f72735646c04f2694ea250348261455c
Bhavik Panchal <32439746+bhavikpanchal28@users.noreply.github.com>
2024-02-06 17:56:02 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app__header"> 
+        <Header />
+
+      </div>
+
+      <div className="app__body">
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
